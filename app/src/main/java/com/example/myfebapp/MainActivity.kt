84d02be085bda550b6261203f9fc4dd5d30913ef
Admin <makansari@gmail.com>
2022-02-03 19:02:07 +0530

    First commit

---
+++ app/src/main/java/com/example/myfebapp/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.myfebapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        buttonSubmit.setOnClickListener {
+           var un = editTextUsername.text.toString()
+
+            Toast.makeText(this, "username is $un", Toast.LENGTH_LONG).show()
+
+            textViewUsername.setText(un)
+        }
+
+    }
+}
