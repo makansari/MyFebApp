e48e9f51c60321fbf7b73902f8000ff8309e47b2
Admin <makansari@gmail.com>
2022-02-04 09:58:06 +0530

    implicit intent and logs and Actvity LC

--- app/src/main/java/com/example/myfebapp/MainActivity.kt
+++ app/src/main/java/com/example/myfebapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myfebapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -10,6 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        var a = 100
+        Log.e("mytag","activity created and data is $a")
 
         buttonSubmit.setOnClickListener {
            /*var un = editTextUsername.text.toString()
@@ -19,9 +23,55 @@ class MainActivity : AppCompatActivity() {
             textViewUsername.setText(un)*/
 
             // Explicit Intent
-            var i = Intent(this, ThirdActivity::class.java)
+            /*var i = Intent(this, ThirdActivity::class.java)
+            startActivity(i)*/
+
+            // Implicit Intent
+//            var i = Intent(Intent.ACTION_DIAL, Uri.parse("tel:9888582252"))
+//            startActivity(i)
+
+            var i = Intent(Intent.ACTION_VIEW, Uri.parse("http:www.ndtv.com"))
             startActivity(i)
         }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        Log.i("mytag","activity started")
+
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        Log.w("mytag","activity resume")
+
+    }
+
+    override fun onPause() {
+        super.onPause()
+        Log.e("mytag","activity paused")
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        Log.i("mytag","activity stopped")
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        Log.i("mytag","activity destroyed")
+
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        Log.i("mytag","activity restarted")
 
     }
 }
